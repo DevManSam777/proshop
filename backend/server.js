f203e06f9b130813b1f01632b37734f672ac7ee6
DevManSam <devmansam@protonmail.com>
2025-01-01 18:27:23 -0800

    added middleware for postman apis

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connetDB from './config/db.js';
-import products from './data/products.js';
 import { doesNotThrow } from 'assert';
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 5000;
 
 connetDB();
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {    
-    res.json(products)
-});  
-
-app.get('/api/products/:id', (req, res) => {        
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
