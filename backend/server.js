bc63ddc668ab00fed5cf243b71ed833e8c737c0f
DevManSam <devmansam@protonmail.com>
2025-01-03 19:58:07 -0800

    user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import connetDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import { doesNotThrow } from 'assert';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 const port = process.env.PORT || 5000;
+
 
 connetDB();
 
@@ -16,6 +18,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
