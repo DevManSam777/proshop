7c4064a099639c8d76b040eff3017266f275adbe
DevManSam <devmansam@protonmail.com>
2025-01-12 18:55:09 -0800

    user api slice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,23 @@
+import { USERS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (data) => ({
+                url: USERS_URL / auth,
+                method: 'POST', 
+                body: data,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        getProductDetails: builder.query({
+            query: (productId) => ({
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5,
+        })
+    }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
