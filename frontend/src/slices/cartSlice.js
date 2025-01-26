3ebf7edb71e901999211e8b816eab95db6439b3b
DevManSam <devmansam@protonmail.com>
2025-01-25 17:50:12 -0800

    payment screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { updateCart } from "../utils/cartUtils"
 
-const initialState =  localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : {cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
+const initialState =  localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 
 
@@ -30,11 +30,15 @@ const cartSlice = createSlice({
         saveShippingAddress: (state, action) => {
             state.shippingAddress = action.payload;
             return updateCart(state);
-        }
+        },
+        savePaymentMethod: (state, action) => {
+            state.paymentMethod = action.payload;
+            return updateCart(state);
+        },
     },
 });
 
 
-export const { addToCart, removeFromCart, saveShippingAddress  } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod  } = cartSlice.actions;
 
 export default cartSlice.reducer;
