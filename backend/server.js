d5a0ffa128fd6b664df03a16bbafdba5b3ef55c4
DevManSam <devmansam@protonmail.com>
2025-02-06 13:58:37 -0800

    production

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,25 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 const __dirname = path.resolve(); // set __dirname to current directory;
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.use(notFound);
-app.use(errorHandler);
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-});
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
+  
+  app.use(notFound);
+  app.use(errorHandler);
+  
+  app.listen(port, () =>
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  );
