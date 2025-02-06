06df763b1055c1a02851e905d9568e6212c3eeb3
DevManSam <devmansam@protonmail.com>
2025-02-06 14:01:52 -0800

    production

--- backend/server.js
+++ backend/server.js
@@ -37,25 +37,23 @@ app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAY
 const __dirname = path.resolve(); // set __dirname to current directory;
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static('/var/data/uploads'));
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
     app.use(express.static(path.join(__dirname, '/frontend/build')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+
+    // any route that is not api will be redirected to index.html
+    app.get('*', (req, res) => 
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
-  } else {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+} else {
     app.get('/', (req, res) => {
-      res.send('API is running....');
+        res.send('API is running...');
     });
-  }
-  
-  app.use(notFound);
-  app.use(errorHandler);
-  
-  app.listen(port, () =>
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
-  );
+};
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
